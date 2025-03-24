8e9e8c5efe53ede8ffce5aa718de0848c6343788
oleg <oleg@botov.org>
2025-03-24 16:33:03 +0300

    flask_restful

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from forms.user import RegisterForm, LoginForm
 from data import db_session, news_api
 from data.news import News
 from data.users import User
+from flask_restful import reqparse, abort, Api, Resource
+from data import news_resources
 
 
 app = Flask(__name__)
+api = Api(app)
+# для списка объектов
+api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+
+# для одного объекта
+api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
